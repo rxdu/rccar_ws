3f970f85f00a67f171a875cb07803fa5b5d82ddb
Ruixiang Du <ruixiang.du@gmail.com>
2022-08-31 23:23:10 +0800

    updated rccar setup

--- .devcontainer/ros_entrypoint.sh
+++ .devcontainer/ros_entrypoint.sh
@@ -1,1 +1,13 @@
-../ros_entrypoint.sh
+#!/bin/bash
+set -e
+
+# setup ros environment
+source "/opt/ros/$ROS_DISTRO/setup.bash"
+
+WS_SETUP_FILE=/ros2_ws/install/setup.bash
+if [ -f "$WS_SETUP_FILE" ]; then
+    echo "Found install/setup.bash"
+    source $WS_SETUP_FILE
+fi
+
+exec "$@"
